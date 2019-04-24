fefb5faedecbf3840810ec7c16277dd2b8368909
nathan-cozens <nathan@numina.co.nz>
2019-04-24 12:28:59 +1200

    any reddit is ready

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -1,14 +1,53 @@
 import React from 'react'
-import {connect} from 'react-redux'
-import {fetchPosts} from '../actions'
+import { connect } from 'react-redux'
+import { fetchPosts } from '../actions'
 
-const LoadSubreddit = ({children, dispatch}) => (
-  <div>
-    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-      Fetch Posts
-    </button>
-    {children}
-  </div>
-)
+// const LoadSubreddit = ({children, dispatch}) => (
+//   <div>
+//     <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+//       Fetch Posts
+//     </button>
+
+//     {children}
+//   </div>
+// )
+
+class LoadSubreddit extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      subreddit: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange() {
+    this.setState({ subreddit: event.target.value });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    const { dispatch } = this.props
+    dispatch(fetchPosts(this.state.subreddit))
+  }
+
+  render() {
+    return (
+
+      <div>
+
+        <input type="text" name='text' onChange={this.handleChange} />
+        <button onClick={this.handleSubmit}>
+          Fetch Posts
+        </button>
+
+
+      </div>
+    )
+  }
+}
+
 
 export default connect()(LoadSubreddit)
